3f65da8ac78b1433857d137721e7237b9e3a9522
Amit-J1000 <amitjadhav.ja1000@gmail.com>
2024-11-22 21:11:07 +0530

    first commit

---
+++ src/main/java/com/csat/user/management/CsatUserManagenentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.csat.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsatUserManagenentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsatUserManagenentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csat/user/management/CsatUserManagenentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csat.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsatUserManagenentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
